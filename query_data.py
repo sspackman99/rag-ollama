9118fde53b47bf6b1c3ee4455682b6c6418c1e55
sam <sspackman99@gmail.com>
2024-10-14 19:26:01 -0600

    fixed for use with stat 66 textbook and ollama

--- query_data.py
+++ query_data.py
@@ -2,6 +2,7 @@ import argparse
 from langchain.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
+import streamlit as st
 
 from get_embedding_function import get_embedding_function
 
@@ -18,13 +19,13 @@ Answer the question based on the above context: {question}
 """
 
 
-def main():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
-    query_rag(query_text)
+# def main():
+#     # Create CLI.
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("query_text", type=str, help="The query text.")
+#     args = parser.parse_args()
+#     query_text = args.query_text
+#     query_rag(query_text)
 
 
 def query_rag(query_text: str):
@@ -40,13 +41,46 @@ def query_rag(query_text: str):
     prompt = prompt_template.format(context=context_text, question=query_text)
     # print(prompt)
 
-    model = Ollama(model="mistral")
+    model = Ollama(model="llama3.2")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
     return response_text
+
+def main():
+    ## Adding a title for the Streamlit app
+    st.title("Ask Me Anything")
+
+    # Adding a subtitle
+    st.subheader("... as long as it is about Stats...")
+
+    # setup a session state message variable to hold all of the session messages
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
+
+    # Build a prompt input for the user
+
+    prompt = st.chat_input('Ask Your Stats Here')
+
+    # If user hits enter then
+    if prompt:
+        # Display prompt
+        st.chat_message('user').markdown(prompt)
+        # store user prompt
+        st.session_state.messages.append({'role':'user', 'content':prompt})
+        # send the prompt to llm
+        response = query_rag(prompt)
+        # show the llm response
+        st.chat_message('assistant').markdown(response)
+        # store the llm response in state
+        st.session_state.messages.append(
+            {'role':'assistant', 'content':response}
+        )
 
 
 if __name__ == "__main__":
